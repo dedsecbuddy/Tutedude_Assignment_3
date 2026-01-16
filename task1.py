be245d2e7d0902620f44cb5f11137e99acb59d4d
dedsecbuddy <81178235+dedsecbuddy@users.noreply.github.com>
2026-01-16 11:39:42 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,9 @@
+def factorial(a):
+    fact = 1
+    for i in range(1,a+1):
+        fact = fact*i
+    return fact
+
+num = int(input("Enter your number: "))
+
+print(f"The factorial of {num} is {factorial(num)}")
